0a66017e647b9b8b3f1461b3550061f8e8aff7e8
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-27 01:54:19 -0600

    small refactor

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ import fs from "fs";
 // Third party
 import inquirer from "inquirer";
 
-const main = async () => {
+/**
+ * Prepares data for inquirer
+ * @returns {Array} dataTransformed
+ */
+const prepData = () => {
   const data = fs.readFileSync("./resources/data.json", "utf8");
   const dataParsed = JSON.parse(data);
   const dataTransformed = dataParsed.map((item) => {
+    // TODO need to get inquirer to ignore period in name as opposed to interpreting it as directory of sorts
     return { ...item, name: item.message, type: "list" };
   });
 
-  const answers = await inquirer.prompt(dataTransformed);
+  return dataTransformed;
+};
+
+const main = async () => {
+  const data = prepData();
+
+  const answers = await inquirer.prompt(data);
 
   console.log(answers);
 };
