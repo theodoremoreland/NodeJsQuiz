9d8e5b390c95f248c7eb974733e4bc259b2be21c
theodoremoreland <theodoremoreland@yahoo.com>
2024-02-22 02:58:01 -0600

    can now start quiz and write input to std in

--- server.js
+++ server.js
@@ -8,11 +8,34 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-wss.on("connection", function connection(ws) {
+// Valid commands
+const up = "\\033[A";
+const down = "\\033[B";
+const left = "\\033[D";
+const right = "\\033[C";
+const enter = "\\r";
+
+wss.on("connection", (ws) => {
+  const ls = spawn("node", ["main.js"]);
+
+  ls.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ls.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ls.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
   ws.on("error", console.error);
 
-  ws.on("message", function incoming(message) {
+  ws.on("message", (message) => {
     console.log("received: %s", message);
+
+    ls.stdin.write(message);
   });
 
   ws.send("something");
